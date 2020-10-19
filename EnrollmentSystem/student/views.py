4c2f1ed6c51b351a032c346221fb09b2aa382940
SAMRAT <samratcse17019@gmail.com>
2020-10-20 01:29:39 +0600

    Add Frontend Design

--- EnrollmentSystem/student/views.py
+++ EnrollmentSystem/student/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from student.models import Students
 from student.forms import studentForm
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     view_student = Students.objects.all()
-    return render(request, 'home.html',{'view_student':view_student})
+    return render(request, 'home.html',{'view_student':view_student, 'title':"Home"})
 
 def addStudent(request):
     if request.method == 'POST':
@@ -19,11 +20,12 @@ def addStudent(request):
 
         student_information = Students(first_name=first_name, last_name=last_name, email=email, phone=phone, address=address, course=course)
         student_information.save()
-    return render(request, 'add_student.html')
+        messages.success(request, "New Student Added")
+    return render(request, 'add_student.html', {'title':"Add Student"})
 
 def studentDetails(request, student_id):
     student_details = Students.objects.get(id=student_id)
-    return render(request, 'student_details.html', {'student_details':student_details})
+    return render(request, 'student_details.html', {'student_details':student_details, 'title':"Student Details"})
 
 def editStudent(request, student_id):
     edit_student = Students.objects.get(id=student_id)
@@ -32,10 +34,12 @@ def editStudent(request, student_id):
         form = studentForm(request.POST, instance=edit_student)
         if form.is_valid():
             form.save()
-    return render(request, 'edit_student.html', {'edit_student':edit_student})
+            messages.success(request, "Updated Successfully!")
+    return render(request, 'edit_student.html', {'edit_student':edit_student, 'title':"Edit Student"})
 
 def deleteStudent(request, student_id):
     delete_student = Students.objects.get(id=student_id)
     delete_student.delete()
+    messages.success(request, "Deleted Successfully!")
     return home(request)
 
